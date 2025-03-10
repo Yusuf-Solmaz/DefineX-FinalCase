d1e48c178bca22349f5e0b4fd81978058b3c19a7
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-03-10 22:28:41 +0300

    feat: add project entity

---
+++ project-service/src/main/java/com/yms/projectservice/entity/Project.java
@@ -0,0 +1,41 @@
+package com.yms.projectservice.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "projects")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Project title cannot be empty.")
+    @Size(min = 3, max = 100, message = "Project title must be between 3 and 100 characters.")
+    @Column(nullable = false)
+    private String title;
+
+    @NotBlank(message = "Description cannot be empty.")
+    @Size(min = 10, message = "Project description must be at least 10 characters long.")
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String description;
+
+    @ElementCollection
+    private List<Long> teamMemberIds;
+
+    private String departmentName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ProjectStatus status;
+}
