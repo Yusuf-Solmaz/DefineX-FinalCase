6dc5e79cd72ab77825242711572b704b45e30a7b
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-03-12 01:00:19 +0300

    feat: add getAllMembers

--- project-service/src/main/java/com/yms/projectservice/service/abstracts/ProjectService.java
+++ project-service/src/main/java/com/yms/projectservice/service/abstracts/ProjectService.java
@@ -1,6 +1,7 @@
 package com.yms.projectservice.service.abstracts;
 
 import com.yms.projectservice.dto.ProjectDto;
+import com.yms.projectservice.dto.MemberResponse;
 import com.yms.projectservice.entity.Project;
 
 import java.util.List;
@@ -11,6 +12,7 @@ public interface ProjectService {
     ProjectDto save(Project project);
     void deleteById(Long id);
     List<ProjectDto> findAll();
-    List<ProjectDto> findByMemberId(Long memberId);
+    List<MemberResponse> getAllMembers(Long projectId);
+    List<Long> getAllMembersId(Long id);
 
 }
