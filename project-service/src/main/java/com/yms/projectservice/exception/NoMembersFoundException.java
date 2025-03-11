5e00041949f55eede16915a356860bc123ed12a2
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-03-12 00:59:41 +0300

    feat: add NoMembersFoundException

---
+++ project-service/src/main/java/com/yms/projectservice/exception/NoMembersFoundException.java
@@ -0,0 +1,7 @@
+package com.yms.projectservice.exception;
+
+public class NoMembersFoundException extends RuntimeException {
+    public NoMembersFoundException(String message) {
+        super(message);
+    }
+}
