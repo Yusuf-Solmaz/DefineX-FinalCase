6e31713048b60d43eb6f03a2070056ae047ddec3
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-03-14 14:27:39 +0300

    feat: add ActivationTokenException

---
+++ auth-service/src/main/java/com/yms/auth_service/exception/ActivationTokenException.java
@@ -0,0 +1,7 @@
+package com.yms.auth_service.exception;
+
+public class ActivationTokenException extends RuntimeException {
+    public ActivationTokenException(String message) {
+        super(message);
+    }
+}
