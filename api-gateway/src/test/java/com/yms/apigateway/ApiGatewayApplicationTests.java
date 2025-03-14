7fb3dd4ee4864f0e0dee626b337f15df5f6aba9b
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-03-14 14:28:13 +0300

    update: add comment line

--- api-gateway/src/test/java/com/yms/apigateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/yms/apigateway/ApiGatewayApplicationTests.java
@@ -3,11 +3,11 @@ package com.yms.apigateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ApiGatewayApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class ApiGatewayApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
