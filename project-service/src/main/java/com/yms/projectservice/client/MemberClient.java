25becb4ce26a50a3e9540877f5f109c1115a80f3
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-03-16 00:39:25 +0300

    feat: update batch method type

--- project-service/src/main/java/com/yms/projectservice/client/MemberClient.java
+++ project-service/src/main/java/com/yms/projectservice/client/MemberClient.java
@@ -1,8 +1,10 @@
 package com.yms.projectservice.client;
 
-import com.yms.projectservice.dto.MemberResponse;
+//import com.yms.projectservice.client.config.FeignClientConfig;
+import com.yms.projectservice.dto.UserResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -11,5 +13,5 @@ import java.util.List;
 public interface MemberClient {
 
     @GetMapping("/batch")
-    List<MemberResponse> findUsersByIds(@RequestParam List<Long> ids);
+    List<UserResponse> findUsersByIds(@RequestParam List<Integer> ids, @RequestHeader("Authorization") String token);
 }
