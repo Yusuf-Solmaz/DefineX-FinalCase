7ccdbff8e613967ccfab94bb047aac8ff4b3369b
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-03-17 18:52:43 +0300

    feat: add global exception

---
+++ comment-service/src/main/java/com/yms/comment_service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.yms.comment_service.exception;
+
+import com.yms.comment_service.exception.root_exception.RootException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler()
+    public ResponseEntity<RootException> handleException(Exception e) {
+        RootException exception = new RootException(
+                HttpStatus.BAD_REQUEST.value(),
+                e.getLocalizedMessage(),
+                System.currentTimeMillis()
+        );
+        return new ResponseEntity<>(exception, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(TaskNotFoundException.class)
+    public ResponseEntity<RootException> handleException(TaskNotFoundException e) {
+        RootException exception = new RootException(
+                HttpStatus.NOT_FOUND.value(),
+                e.getLocalizedMessage(),
+                System.currentTimeMillis()
+        );
+        return new ResponseEntity<>(exception, HttpStatus.NOT_FOUND);
+    }
+
+
+}
