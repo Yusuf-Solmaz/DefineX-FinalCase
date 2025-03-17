36e57000e879b7a05e7c5894abe47989bfb3fd38
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-03-17 18:51:56 +0300

    update: remove not necessary test

--- comment-service/src/test/java/com/yms/comment_service/CommentServiceApplicationTests.java
+++ comment-service/src/test/java/com/yms/comment_service/CommentServiceApplicationTests.java
@@ -3,11 +3,11 @@ package com.yms.comment_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class CommentServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class CommentServiceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
