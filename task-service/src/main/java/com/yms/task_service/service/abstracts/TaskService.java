65851cbeaad57cf482b59593d8ef08f090ce5ddf
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-03-17 16:19:22 +0300

    feat: add token

--- task-service/src/main/java/com/yms/task_service/service/abstracts/TaskService.java
+++ task-service/src/main/java/com/yms/task_service/service/abstracts/TaskService.java
@@ -7,7 +7,7 @@ import com.yms.task_service.entity.TaskStatus;
 import java.util.List;
 
 public interface TaskService {
-    TaskDto save(Task task);
+    TaskDto save(Task task,String token);
     TaskDto findById(Integer id);
     List<TaskDto> findAllByProjectId(Integer projectId);
     void cancelTask(Integer id);
