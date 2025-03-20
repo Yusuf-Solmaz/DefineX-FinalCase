9e81bc588a7d59131a65da7f3078ab19a43f103a
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-03-20 04:37:00 +0300

    feat: add delete comment

--- comment-service/src/main/java/com/yms/comment_service/service/abstracts/CommentService.java
+++ comment-service/src/main/java/com/yms/comment_service/service/abstracts/CommentService.java
@@ -1,12 +1,13 @@
 package com.yms.comment_service.service.abstracts;
 
 import com.yms.comment_service.dto.CommentDto;
-import com.yms.comment_service.dto.CommentResponse;
+import com.yms.comment_service.dto.CommentRequest;
 import com.yms.comment_service.dto.PagedResponse;
 import org.springframework.data.domain.Pageable;
 
 
 public interface CommentService {
-    CommentDto addComment(CommentResponse response, String userEmail,String token);
+    CommentDto addComment(CommentRequest response, String userEmail, String token);
     PagedResponse<CommentDto> getCommentsByTaskId(Integer taskId, Pageable pageable);
+    void deleteComment(String commentId);
 }
