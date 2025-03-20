3e04c1c5b38d3030046a25e3f1302ea0a313d747
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-03-20 17:41:07 +0300

    feat: add builder

--- task-service/src/main/java/com/yms/task_service/dto/UpdateTaskStatusRequest.java
+++ task-service/src/main/java/com/yms/task_service/dto/UpdateTaskStatusRequest.java
@@ -1,7 +1,9 @@
 package com.yms.task_service.dto;
 
 import com.yms.task_service.entity.TaskStatus;
+import lombok.Builder;
 
+@Builder
 public record UpdateTaskStatusRequest(
         TaskStatus status,
         String reason
