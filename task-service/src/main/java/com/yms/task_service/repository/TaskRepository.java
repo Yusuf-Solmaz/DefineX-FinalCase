18e786ee680726f3c39bf11874e1c977a83408e1
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-03-20 04:40:59 +0300

    feat: add get method for not cancelled tasks

--- task-service/src/main/java/com/yms/task_service/repository/TaskRepository.java
+++ task-service/src/main/java/com/yms/task_service/repository/TaskRepository.java
@@ -3,9 +3,15 @@ package com.yms.task_service.repository;
 import com.yms.task_service.dto.TaskDto;
 import com.yms.task_service.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TaskRepository extends JpaRepository<Task,Integer> {
     List<Task> findAllByProjectId(Integer projectId);
+
+    @Query("SELECT t FROM Task t WHERE t.projectId = :projectId AND t.status <> 'CANCELLED'")
+    Optional<Task> findByIdAndNotCancelled(@Param("projectId") Integer projectId);
 }
