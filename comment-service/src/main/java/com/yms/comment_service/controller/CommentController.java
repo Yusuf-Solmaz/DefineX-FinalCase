53c89eda0fbc54c579f7ec08314e1557d69eb0ce
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-03-20 04:34:46 +0300

    feat: add deleteComment

--- comment-service/src/main/java/com/yms/comment_service/controller/CommentController.java
+++ comment-service/src/main/java/com/yms/comment_service/controller/CommentController.java
@@ -1,12 +1,13 @@
 package com.yms.comment_service.controller;
 
 import com.yms.comment_service.dto.CommentDto;
-import com.yms.comment_service.dto.CommentResponse;
+import com.yms.comment_service.dto.CommentRequest;
 import com.yms.comment_service.dto.PagedResponse;
 import com.yms.comment_service.service.abstracts.CommentService;
+import jakarta.servlet.ServletRequest;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -14,7 +15,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
-import java.util.List;
 
 
 @RestController
@@ -37,15 +37,21 @@ public class CommentController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<CommentDto> createTask(@RequestBody CommentResponse comment, @RequestHeader("Authorization") String token,HttpServletRequest request) {
-        CommentDto commentDto = commentService.addComment(comment, hasRole(request),token);
+    public ResponseEntity<CommentDto> createComment(@RequestBody @Valid CommentRequest comment, @RequestHeader("Authorization") String token, HttpServletRequest request) {
+        CommentDto commentDto = commentService.addComment(comment, getAuthenticatedUserEmail(request),token);
 
         return ResponseEntity.created(
                 URI.create("/api/v1/comments"+"/"+commentDto.taskId())
         ).body(commentDto);
     }
 
-    private String hasRole(HttpServletRequest request) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteComment(@PathVariable  String id) {
+        commentService.deleteComment(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    private String getAuthenticatedUserEmail(HttpServletRequest request) {
         return request.getHeader("X-User-Id");
     }
 }
