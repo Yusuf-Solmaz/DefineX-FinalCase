1ba2e490972b393a2425bd599280e79ab8f5732e
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-03-20 04:34:34 +0300

    feat: add validation

--- comment-service/src/main/java/com/yms/comment_service/entity/Comment.java
+++ comment-service/src/main/java/com/yms/comment_service/entity/Comment.java
@@ -1,5 +1,8 @@
 package com.yms.comment_service.entity;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.annotation.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,8 +20,14 @@ public class Comment {
     @Id
     private String id;
 
+    @NotNull(message = "Task ID cannot be null.")
     private Integer taskId;
     private String userEmail;
+
+    @NotBlank(message = "Content cannot be empty.")
+    @Size(min = 10, message = "Content must be at least 10 characters long.")
     private String content;
+
     private LocalDateTime createdAt = LocalDateTime.now();
+    private Boolean isDeleted = false;
 }
