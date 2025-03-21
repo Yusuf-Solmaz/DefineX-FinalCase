854699cdeaf50dcb044431a648cfb69c9d4437b2
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-03-21 14:12:43 +0300

    feat: add update task and get all members

--- task-service/src/main/java/com/yms/task_service/service/abstracts/TaskService.java
+++ task-service/src/main/java/com/yms/task_service/service/abstracts/TaskService.java
@@ -1,18 +1,21 @@
 package com.yms.task_service.service.abstracts;
 
-import com.yms.task_service.dto.TaskDto;
+import com.yms.task_service.dto.TaskResponse;
+import com.yms.task_service.dto.UserResponse;
 import com.yms.task_service.dto.request.TaskRequest;
-import com.yms.task_service.entity.Task;
+import com.yms.task_service.dto.request.TaskUpdateRequest;
 import com.yms.task_service.entity.TaskStatus;
 
 import java.util.List;
 
 public interface TaskService {
-    TaskDto save(TaskRequest task, String token);
-    TaskDto findById(Integer id);
-    List<TaskDto> findAllByProjectId(Integer projectId);
+    TaskResponse save(TaskRequest task, String token);
+    TaskResponse findById(Integer id);
+    List<TaskResponse> findAllByProjectId(Integer projectId);
     void cancelTask(Integer id);
 
-    TaskDto updateTaskStatus(Integer taskId, TaskStatus status, String reason);
-    TaskDto findByIdAndNotCancelled(Integer id);
+    TaskResponse updateTaskStatus(Integer taskId, TaskStatus status, String reason);
+    TaskResponse findByIdAndNotCancelled(Integer id);
+    TaskResponse updateTask(Integer taskId, TaskUpdateRequest request, String token);
+    List<UserResponse> getAllMembers(Integer taskId, String token);
 }
