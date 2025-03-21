a281a6e27e6c6c39c8fd37b668ebf1821e0d1614
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-03-21 23:33:12 +0300

    feat: package change

--- task-service/src/main/java/com/yms/task_service/client/ProjectClient.java
+++ task-service/src/main/java/com/yms/task_service/client/ProjectClient.java
@@ -1,11 +1,10 @@
 package com.yms.task_service.client;
 
-import com.yms.task_service.dto.ProjectResponse;
+import com.yms.task_service.dto.response.ProjectResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
