5e91290e281f2035907bbd4104dc42b2266d0530
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-03-21 23:37:58 +0300

    feat: add validation

--- task-service/src/main/java/com/yms/task_service/dto/request/TaskUpdateRequest.java
+++ task-service/src/main/java/com/yms/task_service/dto/request/TaskUpdateRequest.java
@@ -1,13 +1,18 @@
 package com.yms.task_service.dto.request;
 
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 import java.util.List;
 
 @Builder
 public record TaskUpdateRequest(
+        @Size(min = 3, max = 100, message = "Project title must be between 3 and 100 characters.")
         String title,
+
+        @Size(min = 10, message = "Project description must be at least 10 characters long.")
         String description,
+
         String priority,
         List<Integer> assigneeId,
         String reason
