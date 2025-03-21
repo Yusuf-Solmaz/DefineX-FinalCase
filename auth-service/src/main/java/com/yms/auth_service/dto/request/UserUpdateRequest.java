65fee00c981f8f3fb4e844741e81fb59fcc3248a
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-03-21 21:22:35 +0300

    feat: add validation

--- auth-service/src/main/java/com/yms/auth_service/dto/request/UserUpdateRequest.java
+++ auth-service/src/main/java/com/yms/auth_service/dto/request/UserUpdateRequest.java
@@ -1,9 +1,19 @@
 package com.yms.auth_service.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+
 public record UserUpdateRequest(
+        @Size(min = 3, message = "Name should be 3 characters long minimum")
         String firstname,
+
+        @Size(min = 3, message = "Lastname should be 3 characters long minimum")
         String lastname,
+
+        @Email(message = "Invalid Email Address")
         String email,
+
+        @Size(min = 8, message = "Password should be 8 characters long minimum")
         String password
 ) {
 }
