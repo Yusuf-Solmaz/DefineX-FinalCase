fa59b8848e14bfc96c6cb6e060c091c7e9a7767b
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-03-21 23:35:06 +0300

    feat: add validation

--- project-service/src/main/java/com/yms/projectservice/dto/request/ProjectUpdateRequest.java
+++ project-service/src/main/java/com/yms/projectservice/dto/request/ProjectUpdateRequest.java
@@ -14,6 +14,8 @@ public record ProjectUpdateRequest(
         @Size(min = 3, max = 100, message = "Description title must be between 3 and 100 characters.")
         String description,
         List<Integer> teamMemberIds,
+
+        @Size(min = 2, max = 100, message = "Department name title must be between 2 and 100 characters.")
         String departmentName,
         String projectStatus
 ) {
