b9114a2ec175843dd08825b96fe02b06c1b98ca5
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-03-21 15:42:30 +0300

    feat: add not null validation

--- comment-service/src/main/java/com/yms/comment_service/dto/request/CommentCreateRequest.java
+++ comment-service/src/main/java/com/yms/comment_service/dto/request/CommentCreateRequest.java
@@ -1,12 +1,13 @@
 package com.yms.comment_service.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
 public record CommentCreateRequest(
-        @NotBlank(message = "TaskId cannot be empty.")
+        @NotNull(message = "Task ID cannot be null.")
         Integer taskId,
 
         @NotBlank(message = "Content cannot be empty.")
