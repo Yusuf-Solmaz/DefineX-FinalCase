6393e66d2ce9b4859d76dcea63790c93f1fdeacd
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-03-21 21:18:08 +0300

    feat: add account activation

---
+++ auth-service/src/main/java/com/yms/auth_service/activation/AccountActivation.java
@@ -0,0 +1,66 @@
+package com.yms.auth_service.activation;
+
+import com.yms.auth_service.email.EmailService;
+import com.yms.auth_service.email.EmailTemplateName;
+import com.yms.auth_service.entity.Token;
+import com.yms.auth_service.entity.User;
+import com.yms.auth_service.repository.TokenRepository;
+import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+public  class AccountActivation {
+
+    private final TokenRepository tokenRepository;
+    private final EmailService emailService;
+
+    @Value("${application.mailing.frontend.activation-url}")
+    private String activationUrl;
+
+    public String generateAndSaveActivationToken(User user) {
+
+        String generatedToken = generateActivationCode(6);
+        var token = Token.builder()
+                .token(generatedToken)
+                .createdAt(LocalDateTime.now())
+                .expiresAt(LocalDateTime.now().plusMinutes(15))
+                .user(user)
+                .build();
+        tokenRepository.save(token);
+
+        return generatedToken;
+    }
+
+    public void sendValidationEmail(User user) throws MessagingException {
+        var newToken = generateAndSaveActivationToken(user);
+
+        emailService.sendEmail(
+                user.getEmail(),
+                user.getFullName(),
+                EmailTemplateName.ACTIVATE_ACCOUNT,
+                activationUrl,
+                newToken,
+                "Account activation"
+        );
+    }
+
+    public String generateActivationCode(int length) {
+        String characters = "0123456789";
+        StringBuilder codeBuilder = new StringBuilder();
+
+        SecureRandom secureRandom = new SecureRandom();
+
+        for (int i = 0; i < length; i++) {
+            int randomIndex = secureRandom.nextInt(characters.length());
+            codeBuilder.append(characters.charAt(randomIndex));
+        }
+
+        return codeBuilder.toString();
+    }
+}
