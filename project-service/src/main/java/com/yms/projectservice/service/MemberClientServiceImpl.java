435ac5e831c889d710a9fa06b4b9b4bf603d07e3
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-03-21 23:32:46 +0300

    feat: handle not found and service unavailable errors

--- project-service/src/main/java/com/yms/projectservice/service/MemberClientServiceImpl.java
+++ project-service/src/main/java/com/yms/projectservice/service/MemberClientServiceImpl.java
@@ -3,7 +3,10 @@ package com.yms.projectservice.service;
 import com.yms.projectservice.client.MemberClient;
 import com.yms.projectservice.dto.response.UserResponse;
 import com.yms.projectservice.exception.NoMembersFoundException;
+import com.yms.projectservice.exception.UserServiceUnavailableException;
+import com.yms.projectservice.exception.exception_response.ErrorMessages;
 import com.yms.projectservice.service.abstracts.MemberClientService;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,9 +20,13 @@ public class MemberClientServiceImpl implements MemberClientService {
 
     @Override
     public List<UserResponse> findUsersByIds(List<Integer> ids, String token) {
-        if (ids == null || ids.isEmpty()) {
-            throw new NoMembersFoundException("User ID list is empty.");
+        try{
+            return memberClient.findUsersByIds(ids, token);
+        }catch (FeignException.NotFound e){
+            throw new NoMembersFoundException(ErrorMessages.NO_MEMBERS_FOUND);
         }
-        return memberClient.findUsersByIds(ids, token);
+        catch (RuntimeException e){
+            throw new UserServiceUnavailableException(ErrorMessages.USER_SERVICE_UNAVAILABLE);
+        }
     }
 }
--- task-service/src/main/java/com/yms/task_service/service/MemberClientServiceImpl.java
+++ task-service/src/main/java/com/yms/task_service/service/MemberClientServiceImpl.java
@@ -1,12 +1,14 @@
 package com.yms.task_service.service;
 
 import com.yms.task_service.client.MemberClient;
-import com.yms.task_service.dto.UserResponse;
+import com.yms.task_service.dto.response.UserResponse;
 import com.yms.task_service.exception.NoMembersFoundException;
+import com.yms.task_service.exception.UserServiceUnavailableException;
+import com.yms.task_service.exception.exception_response.ErrorMessages;
 import com.yms.task_service.service.abstracts.MemberClientService;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -17,9 +19,12 @@ public class MemberClientServiceImpl implements MemberClientService {
 
     @Override
     public List<UserResponse> findUsersByIds(List<Integer> ids, String token) {
-        if (ids == null || ids.isEmpty()) {
-            throw new NoMembersFoundException("User ID list is empty.");
+        try{
+            return memberClient.findUsersByIds(ids, token);
+        }catch (FeignException.NotFound e){
+            throw new NoMembersFoundException(ErrorMessages.NO_MEMBERS_FOUND);
+        }catch (Exception e){
+            throw new UserServiceUnavailableException(ErrorMessages.USER_SERVICE_UNAVAILABLE);
         }
-        return memberClient.findUsersByIds(ids, token);
     }
 }
