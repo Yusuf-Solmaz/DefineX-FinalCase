d72cbdc27fff18fa4cd38e77abe0202018254919
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-03-21 21:21:02 +0300

    feat: add validation

--- auth-service/src/main/java/com/yms/auth_service/dto/request/RegistrationRequest.java
+++ auth-service/src/main/java/com/yms/auth_service/dto/request/RegistrationRequest.java
@@ -1,13 +1,34 @@
 package com.yms.auth_service.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Builder
 public record RegistrationRequest(
-    String firstname,
-    String lastname,
-    String email,
-    String password){}
+
+        @NotEmpty(message = "Name is mandatory")
+        @NotNull(message = "Name is mandatory")
+        @Size(min = 3, message = "Name should be 3 characters long minimum")
+        String firstname,
+
+        @Size(min = 3, message = "Lastname should be 3 characters long minimum")
+        @NotEmpty(message = "Lastname is mandatory")
+        @NotNull(message = "Lastname is mandatory")
+        String lastname,
+
+        @Email(message = "Email is not well formatted")
+        @NotEmpty(message = "Email is mandatory")
+        @NotNull(message = "Email is mandatory")
+        String email,
+
+        @NotEmpty(message = "Password is mandatory")
+        @NotNull(message = "Password is mandatory")
+        @Size(min = 8, message = "Password should be 8 characters long minimum")
+        String password) {
+}
 
--- project-service/src/main/java/com/yms/projectservice/dto/request/ProjectUpdateRequest.java
+++ project-service/src/main/java/com/yms/projectservice/dto/request/ProjectUpdateRequest.java
@@ -1,12 +1,17 @@
 package com.yms.projectservice.dto.request;
 
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 import java.util.List;
 
 @Builder
 public record ProjectUpdateRequest(
+
+        @Size(min = 3, max = 100, message = "Project title must be between 3 and 100 characters.")
         String title,
+
+        @Size(min = 3, max = 100, message = "Description title must be between 3 and 100 characters.")
         String description,
         List<Integer> teamMemberIds,
         String departmentName,
