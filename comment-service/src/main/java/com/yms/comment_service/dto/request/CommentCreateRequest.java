83ac08b9300fa50f099b4392adabb476148537e7
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-03-21 15:34:47 +0300

    feat: add validation

--- comment-service/src/main/java/com/yms/comment_service/dto/request/CommentCreateRequest.java
+++ comment-service/src/main/java/com/yms/comment_service/dto/request/CommentCreateRequest.java
@@ -1,10 +1,16 @@
 package com.yms.comment_service.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
 public record CommentCreateRequest(
+        @NotBlank(message = "TaskId cannot be empty.")
         Integer taskId,
+
+        @NotBlank(message = "Content cannot be empty.")
+        @Size(min = 10, message = "Content must be at least 10 characters long.")
         String content
 ) {
 }
