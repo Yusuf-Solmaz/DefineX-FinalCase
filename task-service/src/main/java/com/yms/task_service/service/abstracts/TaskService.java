38a3f3769df75cdf913063c984db30b53693175a
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-03-21 23:37:41 +0300

    feat: change method signature

--- task-service/src/main/java/com/yms/task_service/service/abstracts/TaskService.java
+++ task-service/src/main/java/com/yms/task_service/service/abstracts/TaskService.java
@@ -1,15 +1,15 @@
 package com.yms.task_service.service.abstracts;
 
-import com.yms.task_service.dto.TaskResponse;
-import com.yms.task_service.dto.UserResponse;
-import com.yms.task_service.dto.request.TaskRequest;
+import com.yms.task_service.dto.response.TaskResponse;
+import com.yms.task_service.dto.response.UserResponse;
+import com.yms.task_service.dto.request.TaskCreateRequest;
 import com.yms.task_service.dto.request.TaskUpdateRequest;
 import com.yms.task_service.entity.TaskStatus;
 
 import java.util.List;
 
 public interface TaskService {
-    TaskResponse save(TaskRequest task, String token);
+    TaskResponse save(TaskCreateRequest task, String token);
     TaskResponse findById(Integer id);
     List<TaskResponse> findAllByProjectId(Integer projectId);
     void cancelTask(Integer id);
