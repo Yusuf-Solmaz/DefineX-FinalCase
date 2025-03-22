90ea0b6fa9e59ed8306236290dc9bcdbaf62554e
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-03-22 20:58:19 +0300

    feat: update general url

--- task-service/src/main/java/com/yms/task_service/controller/TaskController.java
+++ task-service/src/main/java/com/yms/task_service/controller/TaskController.java
@@ -1,47 +1,21 @@
 package com.yms.task_service.controller;
 
-import com.yms.task_service.dto.request.TaskCreateRequest;
 import com.yms.task_service.dto.response.TaskResponse;
 import com.yms.task_service.dto.request.TaskStatusUpdateRequest;
 import com.yms.task_service.dto.response.UserResponse;
-import com.yms.task_service.dto.request.TaskUpdateRequest;
 import com.yms.task_service.service.abstracts.TaskService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/tasks")
+@RequestMapping("api/v1/tasks/open")
 @RequiredArgsConstructor
 public class TaskController {
 
     private final TaskService taskService;
-
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<TaskResponse> createTask(
-            @RequestBody @Valid TaskCreateRequest taskCreateRequest,
-            @RequestHeader("Authorization") String token) {
-        TaskResponse savedTask = taskService.save(taskCreateRequest, token);
-        return ResponseEntity.created(
-                URI.create("/api/v1/tasks" + "/" + savedTask.id())
-        ).body(savedTask);
-    }
-
-    @PutMapping("/{taskId}")
-    public ResponseEntity<TaskResponse> updateTask(
-            @PathVariable Integer taskId,
-            @RequestBody @Valid TaskUpdateRequest request,
-            @RequestHeader("Authorization") String token) {
-
-        TaskResponse updatedTask = taskService.updateTask(taskId, request,token);
-        return ResponseEntity.ok(updatedTask);
-    }
 
     @PatchMapping("/{taskId}/status")
     public ResponseEntity<TaskResponse> updateTaskStatus(
@@ -75,12 +49,4 @@ public class TaskController {
         List<TaskResponse> tasks = taskService.findAllByProjectId(projectId);
         return ResponseEntity.ok(tasks);
     }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> cancelTask(@PathVariable Integer id) {
-        taskService.cancelTask(id);
-        return ResponseEntity.noContent().build();
-    }
-
-
 }
