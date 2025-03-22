970bdfb30de4aaa2ed7d2ffcb3ee127914f5b2e8
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-03-22 18:15:17 +0300

    feat: remove open api urls

--- api-gateway/src/main/java/com/yms/apigateway/filter/RouteValidator.java
+++ api-gateway/src/main/java/com/yms/apigateway/filter/RouteValidator.java
@@ -3,35 +3,17 @@ package com.yms.apigateway.filter;
 import com.yms.apigateway.util.Roles;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
-
 import java.util.*;
 import java.util.function.Predicate;
-
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.stereotype.Component;
-
 import java.util.List;
-import java.util.function.Predicate;
 
 @Component
 public class RouteValidator {
 
     public static final List<String> openApiEndpoints = List.of(
-            "/api/v1/auth/**",
-            "/api/v1/search/**",
-            "/v2/api-docs",
-            "/v3/api-docs",
-            "/v3/api-docs/**",
-            "/swagger-resources",
-            "/swagger-resources/**",
-            "/configuration/ui",
-            "/configuration/security",
-            "/swagger-ui/**",
-            "/webjars/**",
-            "/swagger-ui.html"
+            "/api/v1/auth/**"
     );
 
-    // Yetki gerektiren endpoint'ler ve bunlara erişebilen roller
     public static final Map<String, List<String>> authorizationRequiredEndpoints = Map.of(
             "/api/v1/projects", List.of(Roles.MANAGER, Roles.LEADER),
 
