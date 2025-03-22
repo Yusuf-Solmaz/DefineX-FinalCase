d38c730f955b1593554b0be5e4d8f07c87d29d5c
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-03-22 20:56:15 +0300

    feat: add required error messages

--- api-gateway/src/main/java/com/yms/apigateway/exception/exception_response/ErrorMessages.java
+++ api-gateway/src/main/java/com/yms/apigateway/exception/exception_response/ErrorMessages.java
@@ -1,13 +1,7 @@
 package com.yms.apigateway.exception.exception_response;
 
 public class ErrorMessages {
-    public static final String TASK_NOT_FOUND = "Task with ID %d not found!";
-    public static final String PROJECT_SERVICE_UNAVAILABLE = "Project service unavailable.";
-    public static final String FILE_NOT_FOUND = "File name %s not found! (Task with ID %d)";
-    public static final String FILE_COULD_NOT_READ = "File could not be read! Error: %s";
-    public static final String FILE_COULD_NOT_DELETE = "File could not be deleted! Error: %s";
-    public static final String FILE_COULD_NOT_SAVE = "File could not be saved! Error: %s";
-    public static final String UNAUTHORIZED_ENTRY = "Unauthorized entry attempt detected!";
+    public static final String UNAUTHORIZED_ENTRY = "User does not have sufficient roles to access this resource";
     public static final String INVALID_TOKEN = "The provided token is invalid!";
 
     private ErrorMessages() {
