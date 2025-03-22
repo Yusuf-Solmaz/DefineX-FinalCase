6a7cbc91dcfe29d00f9a543a2c0a88d2b0172058
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-03-22 22:28:36 +0300

    feat: add default manager

--- auth-service/src/main/java/com/yms/auth_service/AuthServiceApplication.java
+++ auth-service/src/main/java/com/yms/auth_service/AuthServiceApplication.java
@@ -1,21 +1,28 @@
 package com.yms.auth_service;
 
-//import com.yms.auth_service.entity.Role;
-//import com.yms.auth_service.repository.RoleRepository;
+import com.yms.auth_service.dto.request.RegistrationRequest;
 import com.yms.auth_service.entity.Role;
 import com.yms.auth_service.repository.RoleRepository;
+import com.yms.auth_service.repository.UserRepository;
+import com.yms.auth_service.service.abstracts.AuthenticationService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import static com.yms.auth_service.utils.Role.ROLE_PROJECT_MANAGER;
+
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @EnableAsync
+@RequiredArgsConstructor
 public class AuthServiceApplication {
+
+	private final AuthenticationService authenticationService;
+	private final UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServiceApplication.class, args);
@@ -32,6 +39,16 @@ public class AuthServiceApplication {
 			}
 			if (roleRepository.findByName("ROLE_PROJECT_MANAGER").isEmpty()) {
 				roleRepository.save(Role.builder().name("ROLE_PROJECT_MANAGER").build());
+			}
+			RegistrationRequest request = RegistrationRequest.builder()
+					.firstname("Yusuf")
+					.email("yusuf@gmail.com")
+					.password("123456")
+					.lastname("Solmaz")
+					.build();
+
+			if (userRepository.findByEmail("yusuf@gmail.com").isEmpty()) {
+				authenticationService.register(request,ROLE_PROJECT_MANAGER.name());
 			}
 		};
 	}
