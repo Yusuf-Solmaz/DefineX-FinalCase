3d337d3f8d7c21a5c850708ae8122c3f4fb6a1ac
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-03-22 17:13:55 +0300

    feat: remove unused error messages

--- auth-service/src/main/java/com/yms/auth_service/exception/exception_response/ErrorMessages.java
+++ auth-service/src/main/java/com/yms/auth_service/exception/exception_response/ErrorMessages.java
@@ -1,17 +1,9 @@
 package com.yms.auth_service.exception.exception_response;
 
 public class ErrorMessages {
-    public static final String PROJECT_NOT_FOUND = "Project with ID %d not found!";
-    public static final String NO_MEMBERS_FOUND = "No members found in the project.";
-    public static final String INVALID_PROJECT_STATUS = "Invalid project status value: %s";
-    public static final String INVALID_TEAM_MEMBER = "Invalid team member IDs: ";
-    public static final String PROJECT_STATUS_TRANSITION_INVALID = "Invalid state transition from %s to %s.";
-    public static final String USER_SERVICE_UNAVAILABLE = "User service unavailable.";
-    public static final String CHANGE_TASK_COMPLETED = "Cannot change status of a completed project.";
     public static final String CONSTRAINT_VIOLATION = "Constraint violation errors occurred.";
     public static final String VALIDATION_ERROR = " Validation errors occurred.";
     public static final String BAD_CREDENTIALS_LOGIN = "Login and / or Password is incorrect";
-    public static final String BAD_CREDENTIALS_GENERAL = "Bad Credentials";
     public static final String ROLE_NOT_FOUND = "Role: ID %s not found!";
     public static final String INVALID_TOKEN = "Invalid token";
     public static final String TOKEN_EXPIRED ="Activation token has expired. A new token has been send to the same email address";
