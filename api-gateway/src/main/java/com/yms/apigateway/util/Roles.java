13be1f3b6e89470099367fa6e05c8dbfe020e4a6
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-03-22 18:13:26 +0300

    feat: add private constructor

--- api-gateway/src/main/java/com/yms/apigateway/util/Roles.java
+++ api-gateway/src/main/java/com/yms/apigateway/util/Roles.java
@@ -4,4 +4,6 @@ public class Roles {
     public static String MANAGER = "ROLE_TEAM_MANAGER";
     public static String LEADER = "ROLE_TEAM_LEADER";
     public static String MEMBER = "ROLE_TEAM_MEMBER";
+
+    private Roles() {}
 }
